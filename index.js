2d403f11ca88f8bf08c0a0548d3b0edac2788a39
matthewjburton <mattjburton64@gmail.com>
2024-09-16 11:26:13 -0400

    3.6

--- index.js
+++ index.js
@@ -70,10 +70,16 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (persons.some((person) => person.name === body.name)) {
+    return response.status(409).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
+    id: generateId(),
     name: body.name,
     number: body.number,
-    id: generateId(),
   };
 
   persons = persons.concat(person);
