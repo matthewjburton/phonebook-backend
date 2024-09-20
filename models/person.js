cfdef4ea63b772f1a3e27e44b6ee83debca16721
matthewjburton <mattjburton64@gmail.com>
2024-09-20 09:27:01 -0400

    3.21 & 3.22

--- models/person.js
+++ models/person.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -29,20 +29,20 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function (v) {
         // Regular expression to match numbers in the format xx-xxxxxxx or xxx-xxxxxxx
-        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8
       },
     },
     message: (props) =>
       `${props.value} is not a valid phone number! It should be in the format xx-xxxxxxx or xxx-xxxxxxx`,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
